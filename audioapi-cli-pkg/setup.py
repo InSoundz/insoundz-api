50f5cb77ce4c25cd11ce566c8909ff9c5be98403
Joseph Matan <joseph@insoundz.com>
2022-07-12 13:02:33 +0300

    now we can run audioapi-cli right after doing pip install

--- audioapi-cli-pkg/setup.py
+++ audioapi-cli-pkg/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='audioapi_cli',  
+    name='audioapi-cli',  
     version='0.0.1',
     description="A simple CLI for Insoundz Audio API tool for audio enhancement",
     entry_points={"console_scripts": ["audioapi_cli=cli.audioapi_cli:audioapi_cli"]},
