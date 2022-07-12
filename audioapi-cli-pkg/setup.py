5cc11345e2230e53b03972d6477cbf5bccbc5579
Joseph Matan <joseph@insoundz.com>
2022-07-12 12:30:48 +0300

    WIP: update pkg installation

--- audioapi-cli-pkg/setup.py
+++ audioapi-cli-pkg/setup.py
@@ -4,9 +4,16 @@ setup(
     name='audioapi-cli',  
     version='0.0.1',
     description="A simple CLI for Insoundz Audio API tool for audio enhancement",
+    entry_points={"console_scripts": ["audioapi_cli=audioapi_cli:main"]},
     readme = "README.md",
     license_files = ('LICENSE',),
     python_requires=">=3.7",
     packages=find_packages(),
-    install_requires=['click', 'goto-statement', 'wget', 'boto3', 'audioapi'],
+    install_requires=[
+                        'click',
+                        'goto-statement',
+                        'wget',
+                        'boto3',
+                        'audioapi'
+    ],
  )
