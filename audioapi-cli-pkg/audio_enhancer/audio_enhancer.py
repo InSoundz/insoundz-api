a4ebe5e6fd2a5f0543ca641bb5a46f4b6c92db50
Joseph Matan <joseph@insoundz.com>
2022-07-18 13:46:24 +0300

    WIP: handle <dst> argument

--- audioapi-cli-pkg/audio_enhancer/audio_enhancer.py
+++ audioapi-cli-pkg/audio_enhancer/audio_enhancer.py
@@ -4,7 +4,8 @@ import time
 import wget
 import boto3
 import os
-from urllib.parse import urlparse 
+from urllib.parse import urlparse
+from pathlib import Path
 from audioapi import audioapi
 
 
@@ -44,7 +45,7 @@ class AudioEnhancer(object):
         try:
             self._logger.info(f"Uploading {src}.")
             # assuming a valid path (file with extension)
-            filename = os.path.split(src)[1]
+            filename = os.path.basename(src)
             key = os.path.join(AUDIOAPI_UPLOAD_DIR, filename)
             s3 = boto3.resource("s3")
             s3.meta.client.upload_file(src, BACKET, key)
@@ -75,46 +76,68 @@ class AudioEnhancer(object):
 
         self._logger.debug(f"{src_url} was deleted succesfully.")
 
-    def _create_dst_path(self, src):
+    def _get_default_dst_folder(self):
+        return os.getcwd()
+
+    def _get_default_dst_filename(self, src):
         src_filename_with_ext = os.path.split(src)[1]
         src_filename_not_ext = os.path.splitext(src_filename_with_ext)[0]
-        dst_path = os.path.join(os.getcwd(),
-                                src_filename_not_ext + "_enhanced.wav")
-        return dst_path
+        src_filename_ext = os.path.splitext(src_filename_with_ext)[1]     
+        return src_filename_not_ext + "_enhanced" + src_filename_ext
 
-    def _download_enhanced_file(self, enhanced_file_url, dst_path):
-        self._logger.info(f"Downloading enhanced file to local machine \
-                            at {dst_path}")
-        wget.download(enhanced_file_url, dst_path)
+    def _download_enhanced_file(self, enhanced_file_url, dst):
+        self._logger.info(f"Downloading enhanced file to {dst}")
+        wget.download(enhanced_file_url, dst)
         print()  # An aesthetic linebreak
-        self._logger.info(f"{dst_path} was downloaded succesfully.")
+        self._logger.info(f"{dst} was downloaded succesfully.")
 
-    def _is_url(self, src):
-        return urlparse(src).scheme != "" 
+    def _upload_enhanced_file(self, enhanced_file_url, dst):
+        raise NotImplementedError(f"Uploading enhanced file to {dst} - yet not supported.")
 
-    def enhance_file(
-        self, src, no_download=False, dst_path=None, retention=None
-    ):
+    def _is_url(self, path):
+        if path:
+            return urlparse(path).scheme != ""
+        else:
+            return False 
+
+    def _is_file(self, path):
+        if path:
+            return os.path.splitext(path)[1] != ""
+        else:
+            return False 
+
+    def _is_folder(self, path):
+        if path:
+            return not self._is_file(path)
+        else:
+            return False 
+
+    def enhance_file(self, src, no_download=False, dst=None, retention=None):
         """
         It uses the audioapi package to enhance the file that is located
         in <src>.
         After the audio enhancement process is done, the URL of the new and
         enhanced file will be displayed.
-        The enhanced file will be downloaded to the local machine at
-        <dst_path> unless the <no_download> flag is set.
+        If <dst> is a URL, the enhanced file will be uploaded to this URL.
+        If <dst> is a file or directory path, the enhanced file will be
+        downloaded to the local machine (unless the <no_download> flag is set).
 
         :param str  src:            Contains a URL or a local path of the
                                     original audio file.
         :param bool no_download:    Set this flag if want to skip the download
                                     of the enhanced file to the local machine.
                                     (This param is optional)
-        :param str  dst_path:       The enhanced file will be downloaded to the
-                                    local machine at <dst_path> unless the
-                                    <no_download> flag is set.
-                                    If <dst_path> is missing, the enhanced file
+        :param str  dst:            If <dst> is a URL, the enhanced file will
+                                    be uploaded to this URL.
+                                    If <dst> is a file or directory path, the
+                                    enhanced file will be downloaded to the
+                                    local machine (unless the <no_download>
+                                    flag is set).
+                                    If <dst> is missing, the enhanced file
                                     will be downloaded to the current directory
                                     under the name
-                                    <original_filename>_enhanced.wav.
+                                    <original_filename>_enhanced.wav (unless
+                                    the <no_download> flag is set).
                                     (This param is optional)
         :param int  retention:      The client can request the audioapi to keep
                                     the URL of the enhanced file alive for a
@@ -172,11 +195,36 @@ class AudioEnhancer(object):
                 else:
                     time.sleep(self._status_interval_sec)
 
-            # Downloading enhanced file
-            if status == "done" and not no_download:
-                if not dst_path:
-                    dst_path = self._create_dst_path(src)
-                self._download_enhanced_file(enhanced_file_url, dst_path)
+            # Handle audio enhancement done
+            if status == "done":
+                
+                # Uploading enhanced file
+                if self._is_url(dst):
+                    self._upload_enhanced_file(enhanced_file_url, dst)
+                
+                # Downloading enhanced file
+                elif not no_download:
+                    folder_path = None
+                    file_name = None
+
+                    # <dst> includes the full path (including the filename)
+                    if self._is_file(dst):
+                        folder_path = os.path.split(dst)[0]
+                        file_name = os.path.split(dst)[1]
+                    
+                    # <dst> includes the path (without the filename)
+                    elif self._is_folder(dst):
+                        folder_path = dst
+                        file_name = self._get_default_dst_filename(src)
+                    
+                    # <dst> is None, so we download to default path
+                    else:
+                        folder_path = self._get_default_dst_folder()
+                        file_name = self._get_default_dst_filename(src)
+
+                    Path(folder_path).mkdir(parents=True, exist_ok=True)
+                    dst_path = os.path.join(folder_path, file_name)
+                    self._download_enhanced_file(enhanced_file_url, dst_path)
 
             # Delete the original file URL in case it was created by the script
             if not self._is_url(src):
@@ -187,5 +235,5 @@ class AudioEnhancer(object):
 
     # TODO: need to support this use-case
     def enhance_stream(
-            self, src_stream_type, src, dst_path, rate, chunksize):
+            self, src_stream_type, src, dst, rate, chunksize):
         raise NotImplementedError("'enhance_stream()' yet not supported.")
