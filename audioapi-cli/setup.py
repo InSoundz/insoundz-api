5b45fc9843138a4ee04dc511a24026416041dcbf
Joseph Matan <joseph@insoundz.com>
2022-08-02 17:29:55 +0300

    enfore click install >=8.1.3 (an older version have a bug)

--- audioapi-cli/setup.py
+++ audioapi-cli/setup.py
@@ -15,7 +15,7 @@ setup(
     package_dir={"audioapi_cli": "src"},
     packages=['audioapi_cli'],
     install_requires=[
-                        'click',
+                        'click>=8.1.3',
                         'audioapi'
     ],
  )
