c75950ac23ae279df8581dcda53ee7b126b371b8
Joseph Matan <113097839+JosephInsoundz@users.noreply.github.com>
2022-09-11 21:38:09 +0300

    Update license and readme files (#4)

--- insoundz_cli/setup.py
+++ insoundz_cli/setup.py
@@ -21,14 +21,24 @@ setup(
         "console_scripts":
             ["insoundz_cli=insoundz_cli.cli:insoundz_cli"]
     },
-    readme="README.md",
-    license_files=('LICENSE',),
+    license='MIT',
     python_requires=">=3.7",
     package_dir={"insoundz_cli": "src"},
     packages=['insoundz_cli'],
+    url='https://github.com/InSoundz/insoundz-api/tree/main/insoundz_cli',
     install_requires=[
-                        'click_creds',
-                        'click>=8.1.3',
-                        'insoundz_api'
+        'click_creds',
+        'click>=8.1.3',
+        'insoundz_api'
+    ],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
  )
